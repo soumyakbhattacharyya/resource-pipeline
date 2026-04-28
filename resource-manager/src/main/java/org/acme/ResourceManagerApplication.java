9a20c65a59354130f9b5ab30bbce6e7a83d139f5
soumyak <bhattacharyya.soumyak@gmail.com>
2026-04-28 08:27:38 +0530

    initial commit

---
+++ resource-manager/src/main/java/org/acme/ResourceManagerApplication.java
@@ -0,0 +1,13 @@
+package org.acme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceManagerApplication.class, args);
+	}
+
+}
